ccd6ff659a966a48e86c078b65a68b5e96fcf114
Noor Alfath <alfath.noor17@gmail.com>
2023-08-11 01:03:20 +0700

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    NgbModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'parkiran-booking',
+    appId: '1:872156140408:web:63bfcaa3962fa7d5802e8d',
+    databaseURL: 'https://parkiran-booking-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'parkiran-booking.appspot.com',
+    locationId: 'asia-southeast1',
+    apiKey: 'AIzaSyD-T3nsAHviKJmXmZMhKPyWpR0WSFQEyUM',
+    authDomain: 'parkiran-booking.firebaseapp.com',
+    messagingSenderId: '872156140408',
+  },
+};
